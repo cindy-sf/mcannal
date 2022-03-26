4bee9bebf5082ede1af02f81af83fadaafd0de58
cindy-sf <c.saintfleurant@gmail.com>
2022-03-26 21:53:21 +0100

    feat: adding useWindowSize hook

---
+++ src/hooks/useWindowSize/index.ts
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react'
+
+type WindowsSize = Record<'width' |'height', number | undefined>
+
+export const useWindowSize = (): WindowsSize => {
+  const [windowSize, setWindowSize] = useState<WindowsSize>({
+    width: undefined,
+    height: undefined,
+  })
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      })
+    }
+
+    window.addEventListener("resize", handleResize)
+
+    handleResize()
+
+    return () => window.removeEventListener("resize", handleResize)
+  }, [])
+
+  return windowSize
+}

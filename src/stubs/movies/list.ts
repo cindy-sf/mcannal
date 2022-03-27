08264443b6e40658202945543169d6a48bab0c7d
cindy-sf <c.saintfleurant@gmail.com>
2022-03-27 18:08:11 +0200

    feat: adding stus for movie details and list

---
+++ src/stubs/movies/list.ts
@@ -0,0 +1,92 @@
+import type { DiscoverMoviesApiResponse } from '@src/types/movies' 
+
+export const listStub:DiscoverMoviesApiResponse = {
+  page: 1,
+  total_pages: 32894,
+  total_results: 657873,
+  results: [
+    {
+      adult: false,
+      backdrop_path: "/fOy2Jurz9k6RnJnMUMRDAgBwru2.jpg",
+      genre_ids: [
+        16,
+        10751,
+        35,
+        14
+      ],
+      id: 508947,
+      original_language: "en",
+      original_title: "Turning Red",
+      overview: "Les aventures de Meilin Lee, une jeune adolescente de 13 ans, pleine d’assurance, mais tiraillée entre son image de petite fille modèle aux yeux de sa mère hyper protectrice et le chaos de l’adolescence. Et comme si tous les changements qui s’opèrent en elle ne suffisaient pas, chaque fois qu’elle est débordée par ses émotions - ce qui, pour une ado, arrive quasiment tout le temps - elle se transforme en panda roux géant !",
+      popularity: 9626.136,
+      poster_path: "/jBCUJ90BVXIDSLlXUvgQcj6LOHa.jpg",
+      release_date: "2022-03-10",
+      title: "Alerte rouge",
+      video: false,
+      vote_average: 7.5,
+      vote_count: 1045
+    },
+    {
+      adult: false,
+      backdrop_path: "/uhYpkLSnlOXTQDmNnu1X4MHMt1S.jpg",
+      genre_ids: [
+        28,
+        12,
+        878
+      ],
+      id: 634649,
+      original_language: "en",
+      original_title: "Spider-Man: No Way Home",
+      overview: "Après les évènements liés à l'affrontement avec Mystério, l'identité secrète de Spider-Man a été révélée. Il est poursuivi par le gouvernement américain, qui l'accuse du meurtre de Mystério, et est traqué par les médias. Cet évènement a également des conséquences terribles sur la vie de sa petite-amie M. J. et de son meilleur ami Ned. Désemparé, Peter Parker demande alors de l'aide au Docteur Strange. Ce dernier lance un sort pour que tout le monde oublie que Peter est Spider-Man. Mais les choses ne se passent pas comme prévu et cette action altère la stabilité de l'espace-temps. Cela ouvre le « Multivers », un concept terrifiant dont ils ne savent quasiment rien.",
+      popularity: 7931.213,
+      poster_path: "/sDYuyvhY0FUGs0MIHGKE6H8ETJZ.jpg",
+      release_date: "2021-12-15",
+      title: "Spider-Man: No Way Home",
+      video: false,
+      vote_average: 8.2,
+      vote_count: 10502
+    },
+    {
+      adult: false,
+      backdrop_path: "/ewUqXnwiRLhgmGhuksOdLgh49Ch.jpg",
+      genre_ids: [
+        28,
+        12,
+        35,
+        878,
+        18
+      ],
+      id: 696806,
+      original_language: "en",
+      original_title: "The Adam Project",
+      overview: "Après un atterrissage accidentel en 2022, le pilote de chasse Adam Reed, qui voyage dans le temps, fait équipe avec lui-même, âgé de 12 ans, dans le but de sauver l'avenir.",
+      popularity: 4186.781,
+      poster_path: "/5OCzQvJ0ePPx66OPGVC6ga9bUUQ.jpg",
+      release_date: "2022-03-11",
+      title: "Adam à travers le temps",
+      video: false,
+      vote_average: 7,
+      vote_count: 1219
+    },
+    {
+      adult: false,
+      backdrop_path: "/j2zyoYrWjWyraHMdkqAkSG1MISJ.jpg",
+      genre_ids: [
+        80,
+        9648,
+        53
+      ],
+      id: 414906,
+      original_language: "en",
+      original_title: "The Batman",
+      overview: "Deux années à arpenter les rues en tant que Batman et à insuffler la peur chez les criminels ont mené Bruce Wayne au coeur des ténèbres de Gotham. Avec seulement quelques alliés de confiance parmi le réseau de corruption de la ville composé de responsables et de personnalités de premier plan, le justicier solitaire s’est établi comme l’unique incarnation de la vengeance auprès de ses concitoyens. Quand un tueur vise l’élite de Gotham avec une série de machinations sadiques, une piste d’indices énigmatiques lance le plus grand détective du monde dans une enquête dans le milieu de la pègre, où il rencontre des personnages comme Catwoman, Le Pingouin, Carmine Falcone et Le Riddler. Alors que les preuves s’accumulent et que le plan du malfaiteur devient clair, Batman doit forger de nouvelles relations, démasquer le coupable et rétablir un peu de justice au milieu de l’abus de pouvoir et de corruption qui tourmente Gotham depuis longtemps.",
+      popularity: 3020.924,
+      poster_path: "/t9JGg10CW1DzXEdWL54ewkUko6N.jpg",
+      release_date: "2022-03-01",
+      title: "The Batman",
+      video: false,
+      vote_average: 7.9,
+      vote_count: 2426
+    },
+  ]
+}

b77ed1fff97dd661aa11d0c7d236c88ff8e0622d
cindy-sf <c.saintfleurant@gmail.com>
2022-03-27 23:10:54 +0200

    feat: adding stubs and types for trending shows

---
+++ src/stubs/shows/details.ts
@@ -0,0 +1,57 @@
+import type { MovieDetails } from "@src/types/movies"
+
+export const detailStub: MovieDetails = {
+  adult: false,
+  backdrop_path: "/6mkIV5UmI9CoWZROWDd69KrZuYJ.jpg",
+  belongs_to_collection: null,
+  budget: 0,
+  genres: [
+    {
+      id: 18,
+      name: "Drama"
+    }
+  ],
+  homepage: "http://sonyclassics.com/saintlaurent/",
+  id: 221667,
+  imdb_id: "tt2707848",
+  original_language: "fr",
+  original_title: "Saint Laurent",
+  overview: "1967-1976. As one of history's greatest fashion designers entered a decade of freedom, neither came out of it in one piece.",
+  popularity: 7.94,
+  poster_path: "/xDXKPeCyKuWJIKfFSAYsppKWsgW.jpg",
+  production_companies: [
+    {
+      id: 6896,
+      logo_path: "/2Q89KvzxTKB5BAv9RqFIPI1qb7E.png",
+      name: "EuropaCorp",
+      origin_country: "FR"
+    },
+    {
+      id: 12096,
+      logo_path: null,
+      name: "Mandarin Film",
+      origin_country: "CN"
+    }
+  ],
+  production_countries: [
+    {
+      iso_3166_1: "FR",
+      name: "France"
+    }
+  ],
+  release_date: "2014-09-23",
+  revenue: 0,
+  runtime: 150,
+  spoken_languages: [
+    {
+      iso_639_1: "fr",
+      name: "Français"
+    }
+  ],
+  status: "Released",
+  tagline: "The story that has never been told before",
+  title: "Saint Laurent",
+  video: false,
+  vote_average: 6.2,
+  vote_count: 239
+}
---
+++ src/stubs/shows/list.ts
@@ -0,0 +1,67 @@
+import type { DiscoverShowsApiResponse } from '@src/types/shows' 
+
+export const listStub:DiscoverShowsApiResponse = {
+  page: 1,
+  total_pages: 32894,
+  total_results: 657873,
+  results: [
+    {
+      backdrop_path: "/aqoxEL2k3tao9I8XMeRZRLMbvGH.jpg",
+      first_air_date: "2022-03-10",
+      genre_ids: [
+        35
+      ],
+      id: 158793,
+      name: "Harina",
+      origin_country: [
+        "MX"
+      ],
+      original_language: "es",
+      original_name: "Harina",
+      overview: "",
+      popularity: 2458.611,
+      poster_path: "/7U1yGtb8Oz1Yej5idGh2mKtDQK5.jpg",
+      vote_average: 5.5,
+      vote_count: 4
+    },
+    {
+      backdrop_path: "/hI5h8o3bbZlZwnySEs6rL7pXH32.jpg",
+      first_air_date: "2013-09-12",
+      genre_ids: [
+        18,
+        80
+      ],
+      id: 60574,
+      name: "Peaky Blinders",
+      origin_country: [
+        "GB"
+      ],
+      original_language: "en",
+      original_name: "Peaky Blinders",
+      overview: "En 1919, à Birmingham, soldats, révolutionnaires politiques et criminels combattent pour se faire une place dans le paysage industriel de l'après-Guerre. Le Parlement s'attend à une violente révolte, et Winston Churchill mobilise des forces spéciales pour contenir les menaces. La famille Shelby compte parmi les membres les plus redoutables. Surnommés les \"Peaky Blinders\" par rapport à leur utilisation de lames de rasoir cachées dans leurs casquettes, ils tirent principalement leur argent de paris et de vol. Tommy Shelby, le plus dangereux de tous, va devoir faire face à l'arrivée de Campbell, un impitoyable chef de la police qui a pour mission de nettoyer la ville. Ne doit-il pas se méfier tout autant de la ravissante Grace Burgess ? Fraîchement installée dans le voisinage, celle-ci semble cacher un mystérieux passé et un dangereux secret.",
+      popularity: 2071.217,
+      poster_path: "/bGZn5RVzMMXju4ev7xbl1aLdXqq.jpg",
+      vote_average: 8.6,
+      vote_count: 5790
+    },
+    {
+      backdrop_path: "/4g5gK5eGWZg8swIZl6eX2AoJp8S.jpg",
+      first_air_date: "2003-10-21",
+      genre_ids: [
+        18
+      ],
+      id: 11250,
+      name: "Pasión de gavilanes",
+      origin_country: [
+        "CO"
+      ],
+      original_language: "es",
+      original_name: "Pasión de gavilanes",
+      overview: "",
+      popularity: 1816.545,
+      poster_path: "/lWlsZIsrGVWHtBeoOeLxIKDd9uy.jpg",
+      vote_average: 7.7,
+      vote_count: 1756
+    },
+  ]
+}

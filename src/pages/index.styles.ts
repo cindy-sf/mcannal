23e09947974a7c8ea42144bf695e7ea64502ef53
cindy-sf <c.saintfleurant@gmail.com>
2022-03-28 18:59:22 +0200

    feat: adding dynamic import on home page

--- src/pages/index.styles.ts
+++ src/pages/index.styles.ts
@@ -1,36 +1,6 @@
 import styled from 'styled-components'
 
 export const Badges = styled.div`
-  display flex;
-  margin-top: 5.2rem;
-`
-
-export const MoviCardContainer = styled.div`
   display: flex;
-  margin-top: 2.4rem;
-  padding-bottom: 1.8rem;
-  overflow-x: scroll;
-
-  ::-webkit-scrollbar {
-    width: 0;
-    background: transparent;
-    height: 1.2rem;
-  }
-
-  ::-webkit-scrollbar-thumb {
-    cursor: grab;
-      background: #B6B2B2;
-      border-radius: 1.6rem;
-  }
-  scrollbar-color: #B6B2B2 white;
-  scrollbar-width: thin;
-`
-
-export const Section = styled.section`
-  margin-top: 2.8rem;
-`
-
-export const Title = styled.h2`
-  font-size: 2.8rem;
-  margin-top: 2.6rem;
+  margin-top: 5.2rem;
 `
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import type { NextPage } from 'next'
+import dynamic from 'next/dynamic'
 
 import Badge from '@components/Badge'
+import type { TrendingData } from '@components/DiscoverSection'
 import Layout from '@components/Layout'
-import MovieCard from '@components/MovieCard'
+import Skeleton from '@components/Skeleton'
 import StickyBar from '@components/StickyBar'
 import TextField from '@components/TextField'
 import WelcomeInsert from '@components/WelcomeInsert'
@@ -11,56 +13,55 @@ import WelcomeInsert from '@components/WelcomeInsert'
 import { getTrendings as getTrendingsMovies } from '@src/services/movies/discover'
 import { getTrendings as getTrendingsShows } from '@src/services/shows/discover'
 
-import type { Movies } from '@src/types/movies'
-import type { TvShows } from '@src/types/shows'
+import { Badges } from './index.styles'
 
-import {
-  Badges,
-  MoviCardContainer,
-  Section,
-  Title
-} from './index.styles'
+const DiscoverSection = dynamic(
+  () => import('@components/DiscoverSection'),
+  {
+    loading: () => <Skeleton />,
+  },
+)
 
-// interface TrendingMovies {
-//   popular: Movies[]
-//   upcoming: Movies[]
-//   topRated: Movies[]
-// }
+interface BadgeConfig {
+  title: 'Films' | 'Séries'
+  type: DiscoverType
+}
+
+type DiscoverType = 'movies' | 'tvShows'
+
+const badConfig: BadgeConfig[] = [
+  { title: 'Films', type: 'movies' },
+  { title: 'Séries', type: 'tvShows' },
+]
 
 const Home: NextPage = () => {
-  // const [trendingMovies, setTrendingMovies] = useState<TrendingMovies>()
-  const [popularMovies, setPopularMovies] = useState<Movies[]>()
-  const [upcomingMovies, setUpcomingMovies] = useState<Movies[]>()
-  const [topRatedMovies, setTopRatedMovies] = useState<Movies[]>()
-  const [popularShows, setPopularShows] = useState<TvShows[]>()
-  const [upcomingShows, setUpcomingShows] = useState<TvShows[]>()
-  const [topRatedShows, setTopRatedShows] = useState<TvShows[]>()
+  const [trendingMovies, setTrendingMovies] = useState<TrendingData>()
+  const [trendingShows, setTrendingShows] = useState<TrendingData>()
   
-  const [discoverType, setDiscoverType] = useState<'movies' | 'tvShows'>('movies')
-  const [shouldDisplayError, setShouldDisplayError] = useState<boolean>(false)
+  const [discoverType, setDiscoverType] = useState<DiscoverType>('movies')
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         if (discoverType === 'tvShows') {
-           // has already fetched the data
-          if (upcomingShows?.length && topRatedShows?.length && popularShows?.length) {
-            return
-          }
-          setPopularShows((await getTrendingsShows('popular')).results)
-          setUpcomingShows((await getTrendingsShows('upcoming')).results)
-          setTopRatedShows((await getTrendingsShows('topRated')).results)
-          return
+           // do not fetch again if shows has already been fetched
+          if (trendingShows) return
+
+          setTrendingShows({
+            popular: (await getTrendingsShows('popular')).results,
+            upcoming: (await getTrendingsShows('upcoming')).results,
+            topRated: (await getTrendingsShows('topRated')).results,
+          })
         }
-        // has already fetched the data
-        if (upcomingMovies?.length && topRatedMovies?.length && popularMovies?.length) {
-          return
-        }
-        setPopularMovies((await getTrendingsMovies('popular')).results)
-        setUpcomingMovies((await getTrendingsMovies('upcoming')).results)
-        setTopRatedMovies((await getTrendingsMovies('topRated')).results)
+
+        // do not fetch again if shows has already been fetched
+        if (trendingMovies) return
+        setTrendingMovies({
+          popular: (await getTrendingsMovies('popular')).results,
+          upcoming: (await getTrendingsMovies('upcoming')).results,
+          topRated: (await getTrendingsMovies('topRated')).results,
+        })
       } catch (error) {
-        setShouldDisplayError(true)
         console.error(error)
       }
     }
@@ -75,86 +76,29 @@ const Home: NextPage = () => {
       </StickyBar>
       <WelcomeInsert />
       <Badges>
-        <Badge
-          title="Films"
-          color={discoverType === 'movies' ? 'purple' : 'black'}
-          variant={discoverType === 'movies' ? 'contained' : 'outlined'}
-          onClick={():void => setDiscoverType('movies')}
-        />
-        <Badge
-          title="Séries"
-          color={discoverType === 'tvShows' ? 'purple' : 'black'}
-          variant={discoverType === 'tvShows' ? 'contained' : 'outlined'}
-          onClick={():void => setDiscoverType('tvShows')}
-        />
+        {badConfig.map(({ title, type }) => (
+          <Badge
+            key={title}
+            title={title}
+            color={discoverType === type ? 'purple' : 'black'}
+            variant={discoverType === type ? 'contained' : 'outlined'}
+            onClick={():void => setDiscoverType(type)}
+          />
+        ))}
       </Badges>
-      {discoverType === 'movies' && (
-        <>
-          <Section>
-            <Title>
-              À ne pas manquer
-            </Title>
-            <MoviCardContainer>
-              {popularMovies?.map(movie => (
-                <MovieCard key={movie.id} movie={movie} />
-              ))}
-            </MoviCardContainer>
-          </Section>
-          <Section>
-            <Title>
-              Nouveautés
-            </Title>
-            <MoviCardContainer>
-              {upcomingMovies?.map(movie => (
-                <MovieCard key={movie.id} movie={movie} />
-              ))}
-            </MoviCardContainer>
-          </Section>
-          <Section>
-            <Title>
-              Les mieux notés
-            </Title>
-            <MoviCardContainer>
-              {topRatedMovies?.map(movie => (
-                <MovieCard key={movie.id} movie={movie} />
-              ))}
-            </MoviCardContainer>
-          </Section>
-        </>
+      {discoverType === 'movies' && trendingMovies && (
+        <DiscoverSection
+          popular={trendingMovies.popular}
+          topRated={trendingMovies.topRated}
+          upcoming={trendingMovies.upcoming}
+        />
       )}
-      {discoverType === 'tvShows' && (
-        <>
-          <Section>
-            <Title>
-              À ne pas manquer
-            </Title>
-            <MoviCardContainer>
-              {popularShows?.map(movie => (
-                <MovieCard key={movie.id} movie={movie} />
-              ))}
-            </MoviCardContainer>
-          </Section>
-          <Section>
-            <Title>
-              Nouveautés
-            </Title>
-            <MoviCardContainer>
-              {upcomingShows?.map(movie => (
-                <MovieCard key={movie.id} movie={movie} />
-              ))}
-            </MoviCardContainer>
-          </Section>
-          <Section>
-            <Title>
-              Les mieux notés
-            </Title>
-            <MoviCardContainer>
-              {topRatedShows?.map(movie => (
-                <MovieCard key={movie.id} movie={movie} />
-              ))}
-            </MoviCardContainer>
-          </Section>
-        </>
+      {discoverType === 'tvShows' && trendingShows && (
+        <DiscoverSection
+          popular={trendingShows.popular}
+          topRated={trendingShows.topRated}
+          upcoming={trendingShows.upcoming}
+        />
       )}
     </Layout>
   )
